699c98a20f98d85f0db337d36974504c5a3e53d0
Pawel Bonarek <pawel.bonarek@1gb.pl>
2026-07-02 17:53:09 +0200

    some code clean up

--- lum_plot_heatmap3.py
+++ lum_plot_heatmap3.py
@@ -8,40 +8,40 @@ from PyQt6.QtWidgets import (
     QWidget, 
     QVBoxLayout,
     QHBoxLayout,
-    QGridLayout, 
-    QLabel, 
-    QLineEdit,
-    QGroupBox,
+    QLabel,
     QSlider
 )
 
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
 from matplotlib.backends.backend_qtagg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import matplotlib.cm as cm
+
 import numpy as np
 
 class MplCanvas(FigureCanvasQTAgg):
     """Menages general plot styles"""
+    
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         
+        #create figure
         fig = Figure(
             layout='constrained',
             figsize=(width, height),
             dpi=dpi
             )
 
+        #add main axis (for heatmap)
         self.ax = fig.add_subplot()
 
+        #add aditional axis for line plots
         self.ax_A = self.ax.inset_axes([0, 1.05, 1, 0.25], sharex=self.ax)
         self.ax_B = self.ax.inset_axes([1.05, 0, 0.25, 1], sharey=self.ax)
 
+        #costumise appreance
         self.ax_A.tick_params(axis="both", labelbottom=False, left=False, labelleft=False)
-
         self.ax_B.tick_params(axis="both", labelleft=False, bottom=False, labelbottom=False)
-
         self.ax.ticklabel_format(style='sci',scilimits=(-3,4),axis='both')
         self.ax.yaxis.major.formatter._useMathText = True
         self.ax.xaxis.major.formatter._useMathText = True
@@ -62,8 +62,7 @@ class Heatmap3(QWidget):
             (xmin, xmax) = (0, data.shape[0])
             self.xvalues = range(xmax)
 
-        # Create the maptlotlib FigureCanvas object,
-        # which defines a single set of axes as self.axes.
+        # Create the maptlotlib FigureCanvas object
         self.sc = MplCanvas(self, width=6, height=5, dpi=100)
         self.sc.mpl_connect('motion_notify_event', self.mouse_moved_on_plot)
 
@@ -81,7 +80,7 @@ class Heatmap3(QWidget):
         #update the axis range
         self.sc.ax.set(xlim=(xmin, xmax), ylim=(xmax, xmin))
 
-        #plot lines
+        #plot lines (plot returns a list)
         [self.lineA] = self.sc.ax_A.plot(xvalues, line, color=line_color)
         [self.lineB] = self.sc.ax_B.plot(line, xvalues, color=line_color)
 
@@ -95,16 +94,28 @@ class Heatmap3(QWidget):
         #create toolbar, passing canvas as first parament, parent (self) as second.
         toolbar = NavigationToolbar(self.sc, self)
 
+        #display toolbar above the plot
         L = QVBoxLayout()
         L.addWidget(toolbar)
         L.addWidget(self.sc)
         self.setLayout(L)
+
+    def mouse_moved_on_plot(self, event):
+        """Handles the event of mous moveng across the heatmap.
+        
+        Changes postion of markings on the subplots"""
+        if event.inaxes:
+            self.vline.set_xdata([event.xdata, event.xdata])
+            self.hline.set_ydata([event.ydata, event.ydata])
+            self.sc.figure.canvas.draw_idle()
     
     def redraw(self, data: np.array, line: np.array, line_color):
+        """Redraws the plot with new data"""
         try:
-            #update data
+            #update data on heatmap
             self.plot.set_data(data)
-
+            
+            #update data on line plots
             self.lineA.set(ydata=line, color=line_color)
             self.sc.ax_A.set_ylim(line.min(), line.max())
             
@@ -122,12 +133,7 @@ class Heatmap3(QWidget):
         except TypeError:
 
             return 'Plot update failed: invalid column headings'
-    
-    def mouse_moved_on_plot(self, event):
-        if event.inaxes:
-            self.vline.set_xdata([event.xdata, event.xdata])
-            self.hline.set_ydata([event.ydata, event.ydata])
-            self.sc.figure.canvas.draw_idle()
+
 
 
 class Heatmap3WithSlider(QWidget):
@@ -150,6 +156,7 @@ class Heatmap3WithSlider(QWidget):
             (zmin, zmax) = (0, data.shape[2])
             self.zvalues = range(zmax)
 
+        #preprae colormap for line plots
         self.normalize = mcolors.Normalize(vmin=zmin, vmax=zmax)
         self.colormap = cm.managua_r
 
@@ -157,18 +164,21 @@ class Heatmap3WithSlider(QWidget):
         L0 = QVBoxLayout()
         self.setLayout(L0)
 
-        self.heatmap = Heatmap2(
+        #add the heatmap
+        self.heatmap = Heatmap3(
             self.data[:,:,0], 
             self.lines[:,0], 
             self.colormap(self.normalize(self.zvalues[0])), 
             xvalues=xvalues)
         L0.addWidget(self.heatmap)
 
+        #preper slicer bar
         L1 = QHBoxLayout()
         L1.addWidget(QLabel(str(zmin)))
 
+        #set up horizotal slicer
         slider = QSlider(orientation=Qt.Orientation.Horizontal)
-        slider.setMaximum(0)
+        slider.setMinimum(0)
         slider.setMaximum(len(holder.data.columns)-1)
         slider.valueChanged.connect(self.value_changed)
 
